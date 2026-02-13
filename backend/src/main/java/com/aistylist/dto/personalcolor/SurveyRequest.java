f2341a16638f439b17d20416fade1500750be827
minjunsong <songmj5000@naver.com>
2026-02-13 16:00:11 +0900

    feat: add backend/src/main/java/com/aistylist/dto/personalcolor/SurveyRequest.java

---
+++ backend/src/main/java/com/aistylist/dto/personalcolor/SurveyRequest.java
@@ -0,0 +1,19 @@
+package com.aistylist.dto.personalcolor;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SurveyRequest {
+
+    @NotBlank(message = "설문 답변은 필수입니다")
+    private Map<String, String> answers; // 설문 답변
+}
